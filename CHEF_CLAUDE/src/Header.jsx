5b0f7b2242f39bdad5232d2c6016c088d8f7168a
Supun Fernando <supuncharindu18@gmail.com>
2025-09-27 08:51:57 +0200

    Change some filess and add header funcion for the project

---
+++ CHEF_CLAUDE/src/Header.jsx
@@ -0,0 +1,10 @@
+import chef from "./assets/chef.png"
+
+export default function Header(){
+    return(
+        <header>
+            <img src={chef} alt="chef-icon" />
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
