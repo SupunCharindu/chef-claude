ab90f7b926adeb948f20c5a4bc5729b5abacc997
Supun Fernando <supuncharindu18@gmail.com>
2025-09-27 09:34:11 +0200

    add form section with inpit and add button to main section

--- CHEF_CLAUDE/src/main.jsx
+++ CHEF_CLAUDE/src/main.jsx
@@ -1,3 +1,15 @@
-export default function Main(){
-    
+export default function Main() {
+  return (
+    <main>
+      <form action="" className="add-ingredient-form">
+        <input 
+            type="text" 
+            placeholder="e.g. oregano" 
+            aria-label="Add Ingredient"
+            name="ingredient"
+         />
+         <button>Add Ingredient</button>
+      </form>
+    </main>
+  );
 }
